8e9661e4d10e7aaf506e6fbd052589b2b29fe927
Jordan D'Souza <jordandsouza352@gmail.com>
2021-08-07 16:13:58 +1200

    uninstalled chakra-ui

--- eat-well/src/App.js
+++ eat-well/src/App.js
@@ -1,16 +1,10 @@
-import { ThemeProvider, ColorModeProvider } from "@chakra-ui/react"
-
 function App() {
   return (
-    <ThemeProvider>
       
         <div className="App">
-          <header className="App-header">
-            eat-well
-          </header>
+          
+          
         </div>
-      
-    </ThemeProvider>
   );
 }
 
