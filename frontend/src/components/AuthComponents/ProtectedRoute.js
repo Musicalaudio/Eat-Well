9ecc2d9b4705b90eba6993fe6972b255c299b57a
Jordan D'Souza <jordandsouza352@gmail.com>
2021-09-15 13:18:16 +1200

    created highorder components that protect routes

---
+++ frontend/src/components/AuthComponents/ProtectedRoute.js
@@ -0,0 +1,25 @@
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, verifiedToken, ...rest }) => {
+    return (
+      <Route {...rest} 
+        render = {props => {
+            if (verifiedToken) {
+              return <Redirect to={
+                {
+                    pathname: '/Unauthorized',
+                    state: {
+                    from: props.location
+                }
+            }
+        } />
+            } else {
+              return <Component {...rest} {...props} />
+              
+        }
+    }
+} />
+)
+}
+
+export default ProtectedRoute;
