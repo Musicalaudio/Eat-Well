495dda21071abe614f0d01274422e7d44d9ce188
Jordan D'Souza <jordandsouza352@gmail.com>
2021-09-24 11:00:44 +1200

    created endpoints to save and unsave recipes in our documents

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,4 +1,33 @@
 const router = require('express').Router()
+const User = require('../models/user.model')
+const mongoose = require('mongoose')
+const {Recipe} = require('../models/recipe.model')
+
+router.post('/save-recipe', async(req, res) => {
+    const {userId, id, title, image, imageType} = req.body;
+
+    try{
+        const recipe = {id, title, image, imageType}
+        await User.findOneAndUpdate({_id: userId}, {$push: {savedRecipes: [recipe]}})
+        //console.log("user: ", updatedUser) 
+    }catch(err){
+        console.log(err)
+        res.status(404).json({err})
+    }
+})
+
+
+router.delete('/unsave-recipe/:userId/:id', async(req, res) => {
+    const {userId, id,} = req.params;
+ 
+    try{
+        await User.findOneAndUpdate({_id: userId}, {$pull: {savedRecipes: {id: id}}})
+    }catch(err){
+        console.log(err)
+        res.status(404).json({err})
+    }
+})
+
 
 
 module.exports = router;
