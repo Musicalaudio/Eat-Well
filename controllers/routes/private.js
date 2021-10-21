3874188179cfa546d171f09e16e4d15ad03a4631
Jordan D'Souza <jordandsouza352@gmail.com>
2021-10-22 07:38:46 +1300

    removed dupicates

--- controllers/routes/private.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const { getPrivateRoute } = require('../controllers/private'); 
-const { protect } = require('../middleware/auth')
-
-router.route("/private-screen").get(protect, getPrivateRoute);
-
-module.exports = router;
