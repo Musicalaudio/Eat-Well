5227835faa170270a60a94bdd3dbb9c69bbaf4be
Musicalaudio <jordandsouza352@gmail.com>
2024-02-03 10:39:13 +1300

    removed extra  code

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ const createApp = () => {
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "build", "index.html"));
-    });
   }
   // else{
   //     app.get('/', (req, res) => {
